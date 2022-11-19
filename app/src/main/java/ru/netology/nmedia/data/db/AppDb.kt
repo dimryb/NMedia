1eec3e51461ee41cad19171fbb20c6dc8b4d9bcc
dmitry <dimryb@bk.ru>
2022-11-19 18:26:04 +0700

    Добавил PostRemoteKeyDao

--- app/src/main/java/ru/netology/nmedia/data/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/data/db/AppDb.kt
@@ -3,9 +3,16 @@ package ru.netology.nmedia.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ru.netology.nmedia.data.dao.PostDao
+import ru.netology.nmedia.data.dao.PostRemoteKeyDao
 import ru.netology.nmedia.data.entity.PostEntity
+import ru.netology.nmedia.data.entity.PostRemoteKeyEntity
 
-@Database(entities = [PostEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [PostEntity::class, PostRemoteKeyEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
+    abstract fun postRemoteKeyDao(): PostRemoteKeyDao
 }
